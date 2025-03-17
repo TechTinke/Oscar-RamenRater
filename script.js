c5fad416dd6cbba52cedb1eee07cf1b6bed8e5bf
Oscar Somba <maingioscar2@gmail.com>
2025-03-17 18:09:02 +0300

    Added the Data of the Ramen Dishes through an array

--- script.js
+++ script.js
@@ -10,3 +10,69 @@ document.addEventListener("DOMContentLoaded", () => {
   const deleteBtn = document.getElementById("delete-btn");
   const errorMessage = document.getElementById("error-message");
 });
+const ramens = [
+  {
+    id: 1,
+    name: "Shoyu Ramen",
+    restaurant: "Ichiran",
+    image: "RamenDish 1.jpg",
+    rating: 5,
+    comment: "Delicious!",
+  },
+  {
+    id: 2,
+    name: "Miso Ramen",
+    restaurant: "Menya",
+    image: "RamenDish 2.jpg",
+    rating: 4,
+    comment: "Very flavorful!",
+  },
+  {
+    id: 3,
+    name: "Tonkotsu Ramen",
+    restaurant: "Ramen-ya",
+    image: "RamenDish 3.jpg",
+    rating: 8,
+    comment: "Rich and creamy broth!",
+  },
+  {
+    id: 4,
+    name: "Spicy Ramen",
+    restaurant: "Ichiban",
+    image: "RamenDish 4.jpg",
+    rating: 7,
+    comment: "Super spicy and tasty!",
+  },
+  {
+    id: 5,
+    name: "Shio Ramen",
+    restaurant: "Ramen House",
+    image: "RamenDish 5.jpg",
+    rating: 6,
+    comment: "Light and refreshing!",
+  },
+  {
+    id: 6,
+    name: "Pilau Ramen",
+    restaurant: "Veggie Delight",
+    image: "AddDish 1.jpg",
+    rating: 9,
+    comment: "Best vegetarian option!",
+  },
+  {
+    id: 7,
+    name: "Pork Ramen",
+    restaurant: "Tonkotsu King",
+    image: "AddDish 3.jpg",
+    rating: 10,
+    comment: "Amazing flavor!",
+  },
+  {
+    id: 8,
+    name: "Chicken Tikka Ramen",
+    restaurant: "Tikka King",
+    image: "AddDish 2.jpg",
+    rating: 5,
+    comment: "Nice seafood taste!",
+  },
+];
