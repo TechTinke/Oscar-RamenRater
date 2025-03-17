2b7325219ef86c0d7f9f50a06ea1a77befdc7fa4
Oscar Somba <maingioscar2@gmail.com>
2025-03-17 18:07:42 +0300

    Used DOM to access elements on the html file using their Id

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const ramenContainer = document.getElementById("ramen-container");
+  const detailName = document.getElementById("detail-name");
+  const detailImage = document.getElementById("detail-image");
+  const detailRating = document.getElementById("detail-rating");
+  const detailComment = document.getElementById("detail-comment");
+  const newRatingInput = document.getElementById("new-rating");
+  const newCommentInput = document.getElementById("new-comment");
+  const updateBtn = document.getElementById("update-btn");
+  const deleteBtn = document.getElementById("delete-btn");
+  const errorMessage = document.getElementById("error-message");
+});
